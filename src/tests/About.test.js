cf724f5a586ccd8a671654beadbea7a3f28d0a83
Gustavo <unidragorn@gmail.com>
2022-10-19 17:00:14 -0300

    corrige about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import App from '../App';
+import About from '../pages/About';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Testa o componente <About.js />.', () => {
+describe('Testa componente <About.js />.', () => {
   test('Teste se a página contém as informações sobre a Pokédex', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/About');
+    renderWithRouter(<About />);
     const info = screen.getByText(/This application simulates a Pokédex/i);
     expect(info).toBeDefined();
   });
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/About');
-    const h2 = screen.getByRole('heading', { level: 2, name: /About Pokédex /i });
-    expect(h2).toBeInTheDocument();
+    renderWithRouter(<About />);
+    const header = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(header).toBeInTheDocument();
   });
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/About');
+    renderWithRouter(<About />);
     const paragraph = screen.getByText(/This application simulates/i);
     expect(paragraph).toBeDefined();
   });
   test('Teste se a página contém as informações sobre a Pokédex', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/About');
-    const image = screen.getByAltText(/Pokédex/i).src;
+    renderWithRouter(<About />);
+    const image = screen.getByAltText(/pokédex/i).src;
     expect(image).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<App />);
+    const linkFav = screen.getByText(/Favorite Pokémons/i);
+    userEvent.click(linkFav);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const linkDetails = screen.getByText(/More details/i);
+    userEvent.click(linkDetails);
+    const notFavorite = screen.getByRole('checkbox', { checked: false });
+    userEvent.click(notFavorite);
+    const linkFav = screen.getByText(/Favorite Pokémons/i);
+    userEvent.click(linkFav);
+    const imageCard = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(imageCard);
+  });
+});
