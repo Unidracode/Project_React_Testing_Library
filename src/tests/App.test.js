e4b425cbafeea758f1300c14a32fcf77c3b9c337
Gustavo <unidragorn@gmail.com>
2022-10-19 15:56:12 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { screen } from '@testing-library/react';
+
+describe('Testa Componente <App.js />', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const favoritePokemonsLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+});
