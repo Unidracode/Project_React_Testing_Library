706001b4176e29f84adbf17182888031f4bc66d2
Gustavo <unidragorn@gmail.com>
2022-10-19 17:32:30 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa component <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying/i).src;
+    expect(image).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+})
